291d0dd7b69c7779859709e1a57f06117002da13
Nick Zambo <nzambo@gmail.com>
2022-02-27 17:05:34 -0500

    First

---
+++ Sources/com.scriptblock.aviationwx.sdPlugin/app.js
@@ -0,0 +1,247 @@
+/* global $CC, Utils, $SD */
+
+var uuid;
+
+var canvassi = {};
+
+/**
+ * Here are a couple of wrappers we created to help you quickly setup
+ * your plugin and subscribe to events sent by Stream Deck to your plugin.
+ */
+
+/**
+ * The 'connected' event is sent to your plugin, after the plugin's instance
+ * is registered with Stream Deck software. It carries the current websocket
+ * and other information about the current environmet in a JSON object
+ * You can use it to subscribe to events you want to use in your plugin.
+ */
+
+$SD.on('connected', (jsonObj) => connected(jsonObj));
+
+function connected(jsn) {
+    // Subscribe to the willAppear and other events
+    //com.scriptblock.aviationwx.action.taf
+    //com.scriptblock.aviationwx.action.metar
+
+
+    $SD.on('com.scriptblock.aviationwx.action.metar.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
+    $SD.on('com.scriptblock.aviationwx.action.metar.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
+    $SD.on('com.scriptblock.aviationwx.action.metar.keyDown', (jsonObj) => action.onKeyDown(jsonObj));
+    $SD.on('com.scriptblock.aviationwx.action.metar.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
+    $SD.on('com.scriptblock.aviationwx.action.metar.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
+    $SD.on('com.scriptblock.aviationwx.action.metar.propertyInspectorDidAppear', (jsonObj) => {
+        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
+    });
+    $SD.on('com.scriptblock.aviationwx.action.metar.propertyInspectorDidDisappear', (jsonObj) => {
+        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
+    });
+};
+
+// function processMetar(response, context) {
+//     console.log("got full response from wx api", response);
+//     console.log("going to color filght rules for context: " + context)
+
+//     var c = canvassi[context];
+//     var _ctx = c.getContext("2d");
+
+//     var flightRules = response['flight_rules'];
+//     switch (flightRules) {
+//         case 'IFR':
+//             _ctx.fillStyle = '#DB4F63';
+//             _ctx.fillRect(0,0,c.width, c.height);
+//             break;
+//         case 'LIFR':
+//             _ctx.fillStyle = '#DD50D0';
+//             _ctx.fillRect(0,0,c.width, c.height);
+//             break;
+//         case 'MVFR':
+//             _ctx.fillStyle = '#016AA4';
+//             _ctx.fillRect(0,0,c.width, c.height);
+//             break;
+//         case 'VFR':
+//             _ctx.fillStyle = '#01660B';
+//             _ctx.fillRect(0,0,c.width, c.height);
+//             break;
+
+//     }
+
+//     $SD.api.setImage(context, c.toDataURL());
+
+// }
+
+
+function drawImageOnDeck(context, wxJson) {
+    console.log("in drawImageOnDeck callback");
+    let thisCanvas = canvassi[context];
+    let scene = metarData(thisCanvas, wxJson.data[0].wind.degrees, wxJson.data[0].wind.speed_kts, wxJson.data[0].flight_category, wxJson.data[0], wxJson.data[0].visibility.miles, wxJson.data[0].temperature.fahrenheit)
+
+    $SD.api.setImage(context, scene.toDataURL());
+}
+
+// ACTIONS
+
+const action = {
+    settings:{},
+    onDidReceiveSettings: function(jsn) {
+        // console.log('%c%s', 'color: white; background: red; font-size: 15px;', '[app.js]onDidReceiveSettings:');
+        console.log("NZ: received settings from PI");
+        this.settings = Utils.getProp(jsn, 'payload.settings', {});
+
+        var c = canvassi[jsn.context];
+        var _drawingCtx = c.getContext("2d");
+        _drawingCtx.fillStyle = "#FFFFFF";
+        _drawingCtx.fillRect(0,0,c.width, c.height);
+       $SD.api.setImage(jsn.context, c.toDataURL());
+
+
+        //this.doSomeThing(this.settings, 'onDidReceiveSettings', 'orange');
+
+        /**
+         * In this example we put a HTML-input element with id='mynameinput'
+         * into the Property Inspector's DOM. If you enter some data into that
+         * input-field it get's saved to Stream Deck persistently and the plugin
+         * will receive the updated 'didReceiveSettings' event.
+         * Here we look for this setting and use it to change the title of
+         * the key.
+         */
+
+        //  this.setTitle(jsn);
+    },
+
+    /** 
+     * The 'willAppear' event is the first event a key will receive, right before it gets
+     * shown on your Stream Deck and/or in Stream Deck software.
+     * This event is a good place to setup your plugin and look at current settings (if any),
+     * which are embedded in the events payload.
+     */
+
+    onWillAppear: function (jsn) {
+        console.log("NZ: You can cache your settings in 'onWillAppear'", jsn.payload.settings);
+        /**
+         * The willAppear event carries your saved settings (if any). You can use these settings
+         * to setup your plugin or save the settings for later use. 
+         * If you want to request settings at a later time, you can do so using the
+         * 'getSettings' event, which will tell Stream Deck to send your data 
+         * (in the 'didReceiveSettings above)
+         * 
+         * $SD.api.getSettings(jsn.context);
+        */
+        this.settings = jsn.payload.settings;
+
+        // Nothing in the settings pre-fill, just something for demonstration purposes
+        if (!this.settings || Object.keys(this.settings).length === 0) {
+            this.settings.mynameinput = 'TEMPLATE';
+        }
+        //this.setTitle(jsn);
+        //NZ: probably do the initial call to the api here
+        var _canvas = document.createElement("canvas");
+        // _canvas.height = 144;
+        // _canvas.width = 144;
+        _canvas.height = 98;
+        _canvas.width = 98;
+
+        var _drawingCtx = _canvas.getContext("2d");
+        _drawingCtx.fillStyle = "#FFFFFF";
+        _drawingCtx.fillRect(0,0,_canvas.width, _canvas.height);
+
+        canvassi[jsn.context] = _canvas;
+
+        console.log(this.settings)
+
+        $SD.api.setImage(jsn.context, _canvas.toDataURL());
+
+    },
+
+    onKeyUp: function (jsn) {
+        //this.doSomeThing(jsn, 'onKeyUp', 'green');
+    },
+
+    onKeyDown: function (jsn) {
+        console.log("got key down event from: " + jsn.context);
+
+
+
+        fetchAndPublishMetar(jsn.context, jsn.payload.settings.icao_code, jsn.payload.settings.api_key, drawImageOnDeck);
+
+        // const fetchPromise = fetch("https://avwx.rest/api/metar/" + this.settings.icao_code, 
+        //     {method: 'GET', headers: { 'Authorization': 'BEARER ' + this.settings.api_key, 'Content-Type': 'application/json'}});
+
+        // fetchPromise
+        //     .then( response => {
+        //         if(response.ok) {
+        //             return response.json();
+        //         } else {
+        //             throw new Error({error:{message:"Request Error"}})
+        //         }
+        //     })
+        //     .then( json => {
+        //         console.log("request data as follows:", json)
+        //         return json
+        //     })
+        //     .then (response => processMetar(response, jsn.context))
+        //     .catch (error => {
+        //         console.log(error)
+        //     });
+            
+    },
+
+    onSendToPlugin: function (jsn) {
+        /**
+         * This is a message sent directly from the Property Inspector 
+         * (e.g. some value, which is not saved to settings) 
+         * You can send this event from Property Inspector (see there for an example)
+         */ 
+        console.log("NZ: onSendToPlugin");
+        const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
+        if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+            //this.doSomeThing({ [sdpi_collection.key] : sdpi_collection.value }, 'onSendToPlugin', 'fuchsia');            
+        }
+    },
+
+    /**
+     * This snippet shows how you could save settings persistantly to Stream Deck software.
+     * It is not used in this example plugin.
+     */
+
+    saveSettings: function (jsn, sdpi_collection) {
+        console.log('saveSettings:', jsn);
+        if (sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
+            if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+                this.settings[sdpi_collection.key] = sdpi_collection.value;
+                console.log('NZ: setSettings....', this.settings);
+                $SD.api.setSettings(jsn.context, this.settings);
+            }
+        }
+    },
+
+    /**
+     * Here's a quick demo-wrapper to show how you could change a key's title based on what you
+     * stored in settings.
+     * If you enter something into Property Inspector's name field (in this demo),
+     * it will get the title of your key.
+     * 
+     * @param {JSON} jsn // The JSON object passed from Stream Deck to the plugin, which contains the plugin's context
+     * 
+     */
+
+    // setTitle: function(jsn) {
+    //     if (this.settings && this.settings.hasOwnProperty('mynameinput')) {
+    //         console.log("watch the key on your StreamDeck - it got a new title...", this.settings.mynameinput);
+    //         $SD.api.setTitle(jsn.context, this.settings.mynameinput);
+    //     }
+    // },
+
+    /**
+     * Finally here's a method which gets called from various events above.
+     * This is just an idea on how you can act on receiving some interesting message
+     * from Stream Deck.
+     */
+
+    // doSomeThing: function(inJsonData, caller, tagColor) {
+    //     console.log('%c%s', `color: white; background: ${tagColor || 'grey'}; font-size: 15px;`, `[app.js]doSomeThing from: ${caller}`);
+    //     // console.log(inJsonData);
+    // }, 
+
+
+};
+
---
+++ Sources/com.scriptblock.aviationwx.sdPlugin/utils.js
@@ -0,0 +1,329 @@
+// const font = "12pt Courier";
+const font = "8pt Courier";
+const fontMed = "10pt Courier";
+const fontLg = "12pt Courier";
+var windTextColor = "#000000";
+function drawCloudLayer(canvas, cloud, segSize, iteration) {
+    var context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+
+    context.fillStyle = "lightblue";
+    context.fillRect(0, segSize*iteration, w, segSize);
+
+    if(cloud.code == "OVC") {
+        context.fillStyle = "#FFFFFF";
+        context.fillRect(0, (segSize*iteration), w, segSize);
+        // context.strokeStyle	= "#000000";
+        // context.strokeRect(0, (segSize*iteration), w, segSize);
+    }
+    if(cloud.code == "BKN") {
+        let cloudCount=12;
+        let cloudSpacing = w/cloudCount;
+        let drawCloud = true;	
+
+        for(let i=(cloudSpacing*0.50);i<w;i+=cloudSpacing) {
+
+            if(drawCloud) {
+                context.fillStyle = "#FFFFFF";
+                context.fillRect(i, segSize*iteration+(segSize*0.20), cloudSpacing, segSize*0.6);
+            }
+            drawCloud = !drawCloud;
+
+        }
+    }
+
+    if(cloud.code == "SCT") {
+        let cloudCount=6;
+        let cloudSpacing = w/cloudCount;
+        let drawCloud = true;	
+        for(let i=(cloudSpacing*0.50);i<w;i+=cloudSpacing) {
+            if(drawCloud) {
+                context.fillStyle = "#FFFFFF";
+                context.fillRect(i, segSize*iteration+(segSize*0.20), cloudSpacing, segSize*0.6);
+            }
+            drawCloud = !drawCloud;
+
+        }
+    }
+
+
+    if(cloud.code == "FEW") {
+        let cloudCount=4;
+        let cloudSpacing = w/cloudCount;
+        let drawCloud = true;	
+        for(let i=(cloudSpacing*0.50);i<w;i+=cloudSpacing) {
+            if(drawCloud) {
+                context.fillStyle = "#FFFFFF";
+                context.fillRect(i, segSize*iteration+(segSize*0.20), cloudSpacing, segSize*0.6);
+            }
+            drawCloud = !drawCloud;
+
+        }
+    }
+
+    context.strokeStyle	= "#000000";
+    context.strokeRect(0, (segSize*iteration), w, segSize);
+
+    // context.fillStyle = "#FFFFFF";
+    // context.fillRect(0, (segSize*iteration), w, segSize-1);
+    // context.strokeStyle	= "#000000";
+    // context.strokeRect(0, (segSize*iteration), w, segSize-1);				
+    // context.font = "10pt Courier New";
+    context.font = font;
+    if(cloud.code == "CLR") {
+        context.strokeText(cloud.code, w*0.25, segSize*(iteration+1)-(segSize*0.3));
+    } else {
+        context.strokeText(cloud.feet + " " + cloud.code, w*0.25, segSize*(iteration+1)-(segSize*0.3));
+
+    }
+    return canvas;
+}
+
+function drawClouds(canvas, clouds) {
+    var context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    var wmid = canvas.width/2; //centered horizontally
+    var hmid = canvas.height/2; //centered vertically
+
+    if(!clouds.ceiling && !clouds.clouds) {
+        context.fillStyle = "lightblue";
+        context.fillRect(0, 0, w, hmid-1);
+
+        // context.font = "14pt Courier New";
+        context.font = fontMed;
+        context.strokeStyle = windTextColor;
+        context.strokeText("CLEAR",w*0.30, hmid*0.50);
+        return;
+    }
+
+    if(clouds.ceiling) {
+        context.strokeStyle = "#000000";
+        context.moveTo(0,hmid);
+        context.lineTo(w,hmid);
+        context.stroke();
+        // context.font = "14pt Courier New";
+        context.font = fontMed;        
+        context.strokeStyle = windTextColor;
+        context.strokeText(clouds.ceiling.feet,0,hmid+14);
+        context.strokeText(clouds.ceiling.code,0,hmid+28);
+
+    }
+
+    let segments = clouds.clouds.length;
+    let segSize = hmid/segments
+    var iteration = 0;
+    clouds.clouds.reverse().forEach((cloud) => {
+        canvas = drawCloudLayer(canvas, cloud, segSize, iteration++);
+    })
+    return canvas;
+}
+
+function drawVisibility(canvas, visibility) {
+    var context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    // context.font = "14pt Courier New";
+    context.font = fontMed;
+    context.strokeStyle = windTextColor;
+    context.strokeText(visibility,0,h-4);
+    return canvas;
+
+}
+
+function drawTemperature(canvas, tempF) {
+    var context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    // context.font = "14pt Courier New";
+    context.font = fontMed;
+    context.strokeStyle = windTextColor;
+    context.strokeText(tempF,w-25,h-4);
+    return canvas;
+}
+
+
+function drawFlightConditions(canvas, flightConditions) {
+    console.log("in 'drawFlighrConditions'");
+    var context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    var color;
+    switch(flightConditions) {
+        case "LIFR":
+            windTextColor = "#000000";
+            color = "#ff00ff";
+            break;
+        case "MVFR":
+            windTextColor = "#FFFFFF";
+            color = "#0000ff";
+            break;
+        case "IFR":
+            windTextColor = "#000000";
+            color = "#ff0000";
+            break;
+        case "VFR":
+            windTextColor = "#000000";
+            color = "#00ff00";
+            break;
+    }
+    context.clearRect(0,0,w,h);
+    context.fillStyle = color;
+    context.fillRect(0,0,w,h);
+    console.log("returning canvas:");
+    console.log(canvas);
+    return canvas;
+}
+
+function normalize(val, max, min, scale) { 
+    let ratio = (val - min) / (max - min); 
+    ratio = ratio > 1 ? 1 : ratio;
+    return  ratio*scale;
+}
+
+function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
+    //convert degrees into radians
+    startAngle = startAngle*Math.PI/180;
+    endAngle = endAngle*Math.PI/180;
+    // console.log("drawing pie slice");
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(centerX,centerY);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fillStyle = color;
+
+    ctx.fill();
+}
+
+function drawWind(canvas, origAngle, speed) {
+    console.log("in 'drawWind'");
+    var context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    var wmid = canvas.width/2; //centered horizontally
+    var hmid = canvas.height*0.75; //bottom 3/4th
+
+
+    if(speed == 0) {
+        // context.font = "18pt Courier New";
+        context.font = fontMed;
+        context.strokeText("CALM",wmid-30,hmid+20);
+        return;
+
+    }
+
+    var maxRadius = h*0.23;	
+    // var maxRadius = w<=h ? wmid : hmid;	
+    // console.log("max radius: " + maxRadius);
+
+    var actualRadius = normalize(speed, 20, 0, maxRadius);
+    actualRadius = maxRadius;
+
+    // console.log("normalized radius from " + speed + " :	" + actualRadius);
+
+    var angle
+    if(origAngle >= 90) {
+        angle = origAngle-90;
+    } else {
+        angle = 360-(90-origAngle);
+    }
+    // console.log("shifted angle (-90): " + angle);
+
+    //these are degrees
+    var arcLeft = angle-10;
+    var arcRight = angle+10;
+    var angleAdjust = 2.5;
+    if(speed >= 5) { angleAdjust = 5;}
+    if(speed >= 10) { angleAdjust = 7;}
+    if(speed >= 15) { angleAdjust = 10;}
+    if(speed > 20) { angleAdjust = 12.5;}
+
+
+    arcLeft = angle-angleAdjust;
+    arcRight = angle+angleAdjust;
+    // arcLeft = normalize(angle, 15, 0, spee)*-1;
+    // arcRight = normalize(angle, 15, 0, 15);
+
+    let flagColor = "#00ff00";
+    if(speed>10) flagColor = "#ffff00";
+    if(speed>20) flagColor = "#ff0000";
+    drawPieSlice(context, wmid, hmid,actualRadius, arcLeft, arcRight, flagColor);
+
+
+    context.font = font;
+    oppositeAngle = origAngle > 180 ? origAngle - 180 : 360-(180-origAngle);
+    let info = origAngle + "@" + speed;
+    let textOffset = 0.10;
+    context.strokeStyle = windTextColor;
+    if(oppositeAngle >= 0 && oppositeAngle < 90) {
+        context.strokeText(info, wmid+(wmid*textOffset), hmid-(hmid*textOffset));
+    }
+    if(oppositeAngle >= 90 && oppositeAngle < 180) {
+        context.strokeText(info, wmid+(wmid*textOffset), hmid+(hmid*textOffset));
+    }
+    if(oppositeAngle >= 180 && oppositeAngle < 270) {
+        context.strokeText(info, wmid-(wmid*textOffset), hmid+(hmid*textOffset)+5);
+    }
+    if(oppositeAngle >= 270 && oppositeAngle <= 360) {
+        context.strokeText(info, wmid-(wmid*textOffset), hmid-(hmid*textOffset));
+    }
+    console.log("returning canvas after adding wind");
+    console.log(canvas);
+    return canvas;
+}
+
+// function setup() {
+//     //var _canvas = document.createElement("canvas");
+//     var _canvas = document.getElementById("metar");
+
+//     _canvas.width = 144;
+//     _canvas.height = 144;
+
+//     return _canvas;
+
+// }
+
+function metarData(canvas, windAngle, windSpeed, flightConditions, cloudData, visibility, tempF) {
+    console.log("processing metar data and drawing canvas");
+    canvas = drawFlightConditions(canvas, flightConditions);
+    canvas = drawWind(canvas, windAngle, windSpeed);
+    canvas = drawTemperature(canvas, tempF);
+    canvas = drawClouds(canvas, cloudData);
+    canvas = drawVisibility(canvas, visibility);
+    return canvas;
+}
+
+
+function fetchAndPublishMetar(context, icao, apiKey, callback) {
+    console.log("fetching avwx data in 'fetchandpublishmetar'");
+    // const fetchPromise = fetch("https://avwx.rest/api/metar/" + icao,
+    // {method: 'GET', headers: { 'Authorization': 'BEARER ' + apiKey, 'Content-Type': 'application/json'}});
+
+    const fetchPromise = fetch("https://api.checkwx.com/metar/" + icao + "/decoded", 
+        {method: 'GET', headers: { 'X-API-Key': apiKey, 'Content-Type': 'application/json'}});
+
+    fetchPromise
+        .then( response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw new Error({error:{message:"Request Error"}})
+            }
+        })
+        .then( json => {
+            console.log("request data as follows:", json)
+            return json
+        })
+        .then (response => {
+            callback(context, response);
+        })
+        .catch (error => {
+            console.log(error)
+        });
+
+
+}
